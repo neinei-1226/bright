d06f0158b28cada13610b73bc6ea959fc2b67746
neinei <neinei17549207296@163.com>
2019-09-10 10:56:54 +0800

    inital commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+app.use(express.json())
+app.use(require('cors')())
+app.use('/uploads',express.static(__dirname+'/uploads'))
+
+
+require('./plugins/db')(app)
+require('./routes/admin')(app)
+app.listen(3000,()=>{
+    console.log('http://loclhost:3000')
+}) ; 
